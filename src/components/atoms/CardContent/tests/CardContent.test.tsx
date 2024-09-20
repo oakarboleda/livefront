0511861cccf81fc5b848d93e37264300a4165ab2
Oak Arboleda <oakarboleda@gmail.com>
2024-09-20 11:23:39 -0500

    Update CardContent.test.tsx

--- src/components/atoms/CardContent/tests/CardContent.test.tsx
+++ src/components/atoms/CardContent/tests/CardContent.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
 import { CardContent } from '../CardContent'
 import { render } from '@testing-library/react'
 
 describe('<CardCustom />', () => {
+  const spell = {
+    index: '1',
+    name: 'name',
+    level: 1,
+    school: {
+      name: 'school',
+    },
+    casting_time: 'casting_time',
+    range: 'range',
+    components: ['components'],
+    duration: 'duration',
+  }
   it('renders without crashing', async () => {
     const container = document.createElement('div')
     render(<CardContent spell={spell} />)
