6252a13a00604b53a8f4d9eaebdedeeff00a249b
Oak Arboleda <oakarboleda@gmail.com>
2024-09-22 15:37:49 -0500

    Fixing the last UI issue I couldn't get over

--- src/components/atoms/CardCustom/CardCustom.tsx
+++ src/components/atoms/CardCustom/CardCustom.tsx
@@ -40,14 +40,14 @@ export const CardCustom: React.FC<cardCustomProps> = ({
   // Map size prop to Tailwind classes
   const sizeClass =
     size === 'small'
-      ? 'w-64 h-80' // small size equivalent (example: 16rem x 20rem)
+      ? 'h-80' // small size equivalent (example: 16rem x 20rem)
       : size === 'large'
-        ? 'w-96 h-112' // large size equivalent (example: 24rem x 28rem)
-        : 'w-80 h-96' // default medium size (example: 20rem x 24rem)
+        ? ' h-112' // large size equivalent (example: 24rem x 28rem)
+        : ' h-96' // default medium size (example: 20rem x 24rem)
 
   return (
     <div
-      className={`flex p-8 flex-col ${sizeClass} ${className}`}
+      className={`flex justify-center items-center gap-8 flex-col-1 ${sizeClass} ${className}`}
       onClick={onClick}
       role="button"
       tabIndex={0}
--- src/components/organisms/SpellSection/SpellSection.tsx
+++ src/components/organisms/SpellSection/SpellSection.tsx
@@ -67,7 +67,7 @@ const SpellSection: React.FC<SpellsProps> = () => {
   }
 
   return (
-    <section className="flex flex-col p-15 pb-8 w-full h-full" aria-labelledby="spells-heading">
+    <section className="py-24" aria-labelledby="spells-heading">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 id="spells-heading" className="text-3xl font-bold text-center mb-8 text-gray-800">
           Spells
