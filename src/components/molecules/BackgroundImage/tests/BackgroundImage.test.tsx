28df90b67edde6362d1e4ed0150adff2cce26953
Oak Arboleda <oakarboleda@gmail.com>
2024-09-22 13:39:05 -0500

    Removed ImageCustom, AvatarImage, and BackgroundImage components
    The ImageCustom, AvatarImage, and BackgroundImage components along with their respective tests and storybook files have been removed. This includes the deletion of related interfaces for props and any usage within other components.

--- src/components/molecules/BackgroundImage/tests/BackgroundImage.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { BackgroundImage } from '../BackgroundImage'
-import { render } from '@testing-library/react'
-describe('<BackgroundImage />', () => {
-  it('renders without crashing', async () => {
-    const container = document.createElement('div')
-    render(<BackgroundImage />)
-  })
-})
